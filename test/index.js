299b7747235e64095c0f63a6d7d837954b956871
张嘉华 <admin@cavacn.com>
2015-08-08 19:42:41 +0800

    稍微修改了一下test文件
    稍微修改了一下test文件

--- test/index.js
+++ test/index.js
@@ -27,12 +27,12 @@ cluster.on("log",function(processname, message){
 })
 
 cluster.run("IH1508-1M-RGR",__dirname+"/code.js",{ mqSubscribe: "ZJS-DAY", require:[ __dirname+"/../" ],childMain:[__dirname+"/../"]});
-cluster.run("IH1508-2M-RGR",__dirname+"/code.js",{ mqSubscribe: "ZJS-DAY", require:[ __dirname+"/../" ],childMain:[__dirname+"/../"]});
-cluster.run("IH1508-3M-RGR",__dirname+"/code.js",{ mqSubscribe: "ZJS-DAY", require:[ __dirname+"/../" ],childMain:[__dirname+"/../"]});
-cluster.run("IH1508-4M-RGR",__dirname+"/code.js",{ mqSubscribe: "ZJS-DAY", require:[ __dirname+"/../" ],childMain:[__dirname+"/../"]});
-cluster.run("IH1508-5M-RGR",__dirname+"/code.js",{ mqSubscribe: "ZJS-DAY", require:[ __dirname+"/../" ],childMain:[__dirname+"/../"]});
-cluster.run("IH1508-6M-RGR",__dirname+"/code.js",{ mqSubscribe: "ZJS-DAY", require:[ __dirname+"/../" ],childMain:[__dirname+"/../"]});
-cluster.run("IH1508-7M-RGR",__dirname+"/code.js",{ mqSubscribe: "ZJS-DAY", require:[ __dirname+"/../" ],childMain:[__dirname+"/../"]});
-cluster.run("IH1508-8M-RGR",__dirname+"/code.js",{ mqSubscribe: "ZJS-DAY", require:[ __dirname+"/../" ],childMain:[__dirname+"/../"]});
-cluster.run("IH1508-9M-RGR",__dirname+"/code.js",{ mqSubscribe: "ZJS-DAY", require:[ __dirname+"/../" ],childMain:[__dirname+"/../"]});
-cluster.run("IH1508-10M-RGR",__dirname+"/code.js",{ mqSubscribe: "ZJS-DAY", require:[ __dirname+"/../" ],childMain:[__dirname+"/../"]});
+// cluster.run("IH1508-2M-RGR",__dirname+"/code.js",{ mqSubscribe: "ZJS-DAY", require:[ __dirname+"/../" ],childMain:[__dirname+"/../"]});
+// cluster.run("IH1508-3M-RGR",__dirname+"/code.js",{ mqSubscribe: "ZJS-DAY", require:[ __dirname+"/../" ],childMain:[__dirname+"/../"]});
+// cluster.run("IH1508-4M-RGR",__dirname+"/code.js",{ mqSubscribe: "ZJS-DAY", require:[ __dirname+"/../" ],childMain:[__dirname+"/../"]});
+// cluster.run("IH1508-5M-RGR",__dirname+"/code.js",{ mqSubscribe: "ZJS-DAY", require:[ __dirname+"/../" ],childMain:[__dirname+"/../"]});
+// cluster.run("IH1508-6M-RGR",__dirname+"/code.js",{ mqSubscribe: "ZJS-DAY", require:[ __dirname+"/../" ],childMain:[__dirname+"/../"]});
+// cluster.run("IH1508-7M-RGR",__dirname+"/code.js",{ mqSubscribe: "ZJS-DAY", require:[ __dirname+"/../" ],childMain:[__dirname+"/../"]});
+// cluster.run("IH1508-8M-RGR",__dirname+"/code.js",{ mqSubscribe: "ZJS-DAY", require:[ __dirname+"/../" ],childMain:[__dirname+"/../"]});
+// cluster.run("IH1508-9M-RGR",__dirname+"/code.js",{ mqSubscribe: "ZJS-DAY", require:[ __dirname+"/../" ],childMain:[__dirname+"/../"]});
+// cluster.run("IH1508-10M-RGR",__dirname+"/code.js",{ mqSubscribe: "ZJS-DAY", require:[ __dirname+"/../" ],childMain:[__dirname+"/../"]});
