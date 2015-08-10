11de050ab78ddbe61f5f6a6e550ec472830cfe7e
张嘉华 <admin@cavacn.com>
2015-08-10 15:59:47 +0800

    修改测试代码
    修改测试代码

--- test/code.js
+++ test/code.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 量化.模型.继承({
 	执行:function( current ){
 		var ma5 =  当前.简单移动平均(当前.最高价,5)();
 		当前.打印(ma5);
-=======
-Quant.Model.extends({
-	nextTick:function( current ){
-		//this.llv("$.close",5)();
-		this.log(this.llv("$.close",5)(),current.close);
->>>>>>> origin/master
 	}
 })
