3c890849e8e1ab23cd8ffe71b16cfbb33aa73896
NumberT <157943945@qq.com>
2015-08-10 14:59:20 +0800

    增加LLV函数
    增加LLV函数

--- test/code.js
+++ test/code.js
@@ -1,5 +1,6 @@
 Quant.Model.extends({
 	nextTick:function( current ){
-		this.log(this.ma(this.hhv("$.close",5),5)(),current.close);
+		//this.llv("$.close",5)();
+		this.log(this.llv("$.close",5)(),current.close);
 	}
 })
