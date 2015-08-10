7029a7afd78b6247e1e09ea6fca132450073a1cf
张嘉华 <admin@cavacn.com>
2015-08-10 14:25:39 +0800

    添加中文支持
    添加中文支持

--- test/code.js
+++ test/code.js
@@ -1,6 +1,7 @@
-Quant.Model.extends({
-	nextTick:function( current ){
+量化.模型.继承({
+	执行:function( current ){
 		var ma5 =  当前.简单移动平均(当前.最高价,5)();
-		this.log(ma5);
+		当前.打印(ma5);
+		当前.打印("FUCK");
 	}
 })
