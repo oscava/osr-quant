7a26a5cee18faa08f80897ef12ddf78e9f1c6ad0
NumberT <157943945@qq.com>
2015-08-11 10:16:29 +0800

    添加函数cross,ma,hhv,sum,and so on
    添加函数cross,ma,hhv,sum,and so on

--- test/code.js
+++ test/code.js
@@ -1,6 +1,9 @@
 Quant.Model.extends({
-	nextTick:function( current ){
-		//this.llv("$.close",5)();
-		this.log(this.llv("$.close",5)(),current.close);
-	}
-})
+    nextTick: function (current) {
+        //this.llv("$.close",5)();
+        //this.log(this.slope('$.close',5)(), current.close, current.open);
+        this.log("-->" + this.slope('$.close', 5)()/*,current.close*/);
+        //this.log("-->" + this.sma('$.close', 5,3)()/*,current.close*/);
+        //this.log("++>" + this.mema('$.close', 5,1)()/*,current.close*/);
+    }
+});
