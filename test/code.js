029a9b74cbcf4b1140ee591a8a172618daafac6b
张嘉华 <admin@cavacn.com>
2015-08-11 16:04:30 +0800

    出现BUG，因为GIT没用好的原因
    出现BUG，因为GIT没用好的原因

--- test/code.js
+++ test/code.js
@@ -2,8 +2,7 @@ Quant.Model.extends({
     nextTick: function (current) {
         //this.llv("$.close",5)();
         //this.log(this.slope('$.close',5)(), current.close, current.open);
-        this.log("-->" + this.slope('$.close', 5)()/*,current.close*/);
-        //this.log("-->" + this.sma('$.close', 5,3)()/*,current.close*/);
+        this.log("-->" + this.sma('$.close', 5,3)()/*,current.close*/);
         //this.log("++>" + this.mema('$.close', 5,1)()/*,current.close*/);
     }
 });
