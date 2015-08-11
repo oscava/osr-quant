bfd01025dae7d20744718a58b1c4e7b8191dacfb
NumberT <157943945@qq.com>
2015-08-11 10:22:32 +0800

    Revert "添加函数cross,ma,hhv,sum,and so on"
    This reverts commit 7a26a5cee18faa08f80897ef12ddf78e9f1c6ad0.
    
    Conflicts:
    	lib/fns/hhv.js
    	lib/fns/ma.js
    	test/code.js

--- test/code.js
+++ test/code.js
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 量化.模型.继承({
 	执行:function( current ){
 		var ma5 =  当前.简单移动平均(最高价,5)();
 		当前.打印(ma5);
 	}
 })
+=======
+Quant.Model.extends({
+	nextTick:function( current ){
+		//this.llv("$.close",5)();
+		this.log(this.llv("$.close",5)(),current.close);
+	}
+})
+>>>>>>> parent of 7a26a5c... 添加函数cross,ma,hhv,sum,and so on
