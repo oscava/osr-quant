9b57ee28107d3c8d6b936dd0fc7b6deae6ba27f3
张嘉华 <admin@cavacn.com>
2015-08-17 16:52:45 +0800

    增加检查数据接口，防止内存溢出
    增加检查数据接口，防止内存溢出

--- test/code.js
+++ test/code.js
@@ -1,10 +1,9 @@
 量化.模型.继承({
 	名字:	"测试案例1",
 	optdata:function( datas ){
-		console.log("优化数据");
 		return datas;
 	},
 	执行: function( current ){
-		this.buy(3);
+		this.draw("MACLOSE5",this.ma(收盘价,5));
 	}
 });
