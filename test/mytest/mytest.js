c31f712de192bea838d9b208866085b692df6aab
张嘉华 <admin@cavacn.com>
2015-11-08 23:13:24 +0800

    测试原形，该方法通过
    测试原形，该方法通过

---
+++ test/mytest/mytest.js
@@ -0,0 +1,44 @@
+var co = require("co");
+var isFunction = function(obj){
+    return "function" == typeof(obj);
+}
+var abs = function ( express, index ){
+    var _this = this;
+    return function(cb){
+        if(isFunction(express)){
+            // console.log(express._method.())
+            middlewear.bind(_this)(express,index,cb);
+        }else{
+            cb(null, Math.abs(express));
+        }
+    }
+}
+
+var ma = function( express, number, index ){
+    
+    var fn = function( cb ){
+        cb( null, express+number );
+    }
+    
+    fn._args = arguments;
+    fn._method = ma;
+    
+    return fn;
+}
+
+var middlewear = function(express,index,callback){
+    var args = Array.prototype.slice.apply(express._args);
+    args.concat(index);
+    var method = express._method.apply(this,args);
+    method(callback);
+}
+
+co(function *(){
+    var result = yield abs(ma(5,8),1);
+    console.log(result);
+}).then(function( msg ){
+    console.log("ok",msg);
+},function(err){
+    console.log("err",err);
+})
+
