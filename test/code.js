210164f4a3d2c7d8af2081d5db5abe53575c1b1b
张嘉华 <admin@cavacn.com>
2015-11-09 17:36:15 +0800

    这一稿，废的

--- test/code.js
+++ test/code.js
@@ -3,9 +3,13 @@
 #include log
 #include math
 
-var abs_close = yield math.abs( -(yield math.abs(CLOSE)) );
+// var abs_close = yield math.abs( -(yield math.abs(CLOSE)) );
 
-log.info(abs_close);
+// log.info(abs_close,current);
+
+var close_ma5 = yield math.ma( CLOSE , 5 );
+
+log.info(close_ma5);
 
 // log.info(abs_close);
 // log.info("===",yield math.abs( CLOSE ));
