ec2e331cf49982a2151bb11592f120db7fb44d25
张嘉华 <admin@cavacn.com>
2015-11-10 12:04:23 +0800

    重构math函数
    重构math函数

--- test/code.js
+++ test/code.js
@@ -7,9 +7,15 @@
 
 // log.info(abs_close,current);
 
-var close_ma5 = yield math.ma( CLOSE , 5 );
-
-log.info(close_ma5);
-
+// var close_5 = yield math.ABS( CLOSE );
+// log.info("ABS_CLOSE_5\t",close_5);
+// var ma_5 = yield math.MA(math.ABS( OPEN ),5);
+// log.info("MA_CLOSE_5\t",ma_5);
+// var ema_open_5 = yield math.EMA( math.ABS(OPEN), 5);
+// log.info("EMA_OPEN_5\t",ema_open_5,'\t',current.open);
 // log.info(abs_close);
 // log.info("===",yield math.abs( CLOSE ));
+
+var hhv_5 = yield HHV(CLOSE,5);
+var llv_5 = yield LLV(CLOSE,5);
+log.info(hhv_5,"\t",llv_5,"\t",current.close);
